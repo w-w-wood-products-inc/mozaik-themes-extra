63a6e22a7187c7cc7445410702db205ec6c0d554
Andrew Smith <smith.andrew.l@gmail.com>
2019-06-16 12:17:52 -0500

    Added a new theme called wood.

---
+++ src/wood/colors.js
@@ -0,0 +1,10 @@
+export default {
+    text: '#8c8c8c',
+    textHighlight: '#5a5a5a',
+    background: '#000000',
+    icon: '#dfa626',
+    unknown: '#4d2c2e',
+    success: '#4ec2b4',
+    warning: '#d1be65',
+    failure: '#de5029',
+}
