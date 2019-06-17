76617d04bf217e8edc4afe8ce3cc44edaa248b22
Jake Byrd <byrdjake126@gmail.com>
2019-06-17 09:51:49 -0500

    export night green theme

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as testGreen } from './test-green';
+export { default as nightGreen } from './nightgreen';
 export { default as sunsetTheme} from './sunset';
 export { default as woodTheme} from './wood';
