483bdf574a4510257cd4298df5b9afe1cfac63ea
Jake Byrd <byrdjake126@gmail.com>
2019-06-18 13:52:36 -0500

    blue theme added

---
+++ src/blue/charts.js
@@ -0,0 +1,20 @@
+import colors from './colors'
+
+export default {
+    axis: {
+        textColor: colors.text,
+        tickColor: colors.text,
+        legendColor: colors.text,
+    },
+    grid: {
+        stroke: '#0090ff',
+    },
+    colors: ['#3656af', '#0035c6', '#01227f', '#608bff', '#2660ff'],
+    tooltip: {
+        background: '#767777',
+        color: '#0090ff',
+        fontSize: '1.4vmin',
+        borderRadius: '2px',
+        boxShadow: '0 1px 3px rgba(0, 0, 0, 0.5)',
+    },
+}
---
+++ src/blue/colors.js
@@ -0,0 +1,10 @@
+export default {
+    text: '#0090ff',
+    textHighlight: '#0090ff',
+    background: '#767777',
+    icon: '#0090ff',
+    unknown: '#003fff',
+    success: '#4ec26e',
+    warning: '#d1be65',
+    failure: '#de5029',
+}
