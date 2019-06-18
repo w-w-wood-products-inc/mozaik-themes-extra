f4d9d17f557591f2a57606f5bd140cf6a74654ed
Jake Byrd <byrdjake126@gmail.com>
2019-06-18 13:55:37 -0500

    add blue theme as export

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as nightGreen } from './nightgreen';
 export { default as sunsetTheme } from './sunset';
 export { default as woodTheme } from './wood';
 export { default as spiceBush } from './spicebush';
+export { default as blueTheme } from './blue';
