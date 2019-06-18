86d7971e959601d515765480af66870b16ff91bc
Jake Byrd <byrdjake126@gmail.com>
2019-06-18 09:58:05 -0500

    export spicebush theme

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as nightGreen } from './nightgreen';
-export { default as sunsetTheme} from './sunset';
-export { default as woodTheme} from './wood';
+export { default as sunsetTheme } from './sunset';
+export { default as woodTheme } from './wood';
+export { default as spiceBush } from './spicebush';
