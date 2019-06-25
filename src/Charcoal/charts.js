59d1771186f088d65fb72224000c8cf7bc48935c
Andrew Smith <andrew.smith@wwinc.com>
2019-06-25 08:50:54 -0500

    Copied the blue theme into a new charcoal theme.

---
+++ src/Charcoal/charts.js
@@ -0,0 +1,20 @@
+import colors from './colors'
+
+export default {
+    axis: {
+        textColor: colors.text,
+        tickColor: colors.text,
+        legendColor: colors.text,
+    },
+    grid: {
+        stroke: '#003fff',
+    },
+    colors: ['#3656af', '#0035c6', '#c4d3ff', '#608bff', '#2660ff'],
+    tooltip: {
+        background: '#767777',
+        color: '#84b4dc',
+        fontSize: '1.4vmin',
+        borderRadius: '2px',
+        boxShadow: '0 1px 3px rgba(0, 0, 0, 0.5)',
+    },
+}
---
+++ src/Charcoal/colors.js
@@ -0,0 +1,10 @@
+export default {
+    text: '#84b4dc',
+    textHighlight: '#84b4dc',
+    background: '#767777',
+    icon: '#0090ff',
+    unknown: '#003fff',
+    success: '#4ec26e',
+    warning: '#d1be65',
+    failure: '#de5029',
+}
